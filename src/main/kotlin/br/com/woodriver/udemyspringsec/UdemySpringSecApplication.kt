819d972eb52045c44fb0ee9ef9bde8f6fc7476a5
Yan Tapajós <ybrandao.d@gmail.com>
2024-08-24 02:11:01 +0100

    First project commit

---
+++ src/main/kotlin/br/com/woodriver/udemyspringsec/UdemySpringSecApplication.kt
@@ -0,0 +1,11 @@
+package br.com.woodriver.udemyspringsec
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UdemySpringSecApplication
+
+fun main(args: Array<String>) {
+    runApplication<UdemySpringSecApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/woodriver/udemyspringsec/UdemySpringSecApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.woodriver.udemyspringsec
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UdemySpringSecApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
